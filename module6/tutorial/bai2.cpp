6f95b925a2f4e816ebe2d0063ca8e62c1ffdbb7a
Stephenlao <laovinhkhang2002@gmail.com>
2023-11-22 11:46:51 +0700

    fix bai2 module 6 inheritance

--- module6/tutorial/bai2.cpp
+++ module6/tutorial/bai2.cpp
@@ -1,181 +1,181 @@
-    #include <iostream>
-    #include <vector>
-    #include <limits>
-    #include <fstream>
-    using namespace std;
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::string;
+using std::vector;
+using std::fstream;
 
-    class Student {
-        private:
-            string name;
-            int studentID;
-        
-        public:
-        // initialize constructor as member initializer
-            Student(string name_val = "", int studentID = 0) : name(name_val), studentID(studentID) {
-            };
-
-        // destructor when object is out of scope
-            ~Student() {
-            };
-
-            void setName(string name_val) {
-                this -> name = name_val;
-            };
-
-            string getName() {
-                return name;
-            };
-
-            void setStudentID(int studentID) {
-                this ->studentID = studentID;
-            };
-
-            int getStudentID() {
-                return studentID;
-            };
-    // A virtual function in C++ is a member function of a base class that can be overridden in a derived class. 
-    // The primary purpose of virtual functions is to support polymorphism, 
-    // allowing a base class pointer (or reference) to call the appropriate function depending on the type of the object it points to (or references).
-            virtual string toString() {
-                return "Name: " + name + " "+ "StudentID: " + to_string(studentID);
-            };
-    };
-
-    class SchoolSystem {
-        private:
-        std::vector<Student> students;
-        ifstream infile;
-
-        public:
-            SchoolSystem() {};
-            void addStudent() { 
-                string name;
-                int studentID;  
-                cout <<"Enter student name: ";
-                cin.ignore(1, '\n'); // ignore the '\n'
-                getline(cin, name); // because name has space => get all line
-
-                cout <<"Enter studentID: ";
-                cin >> studentID;
-                Student student(name, studentID);
-                students.push_back(student);
-                cin.clear(); // clear student object to avoid an error: ex: when saving student object in students vector array
-                // it brings this array and come back to while loop and assign it to 'choice' variable which is invalid input
-                // because choice is integer. Thus we need to clear input 
-                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // discard invalid input
-            };
-
-            void removeStudent() {
-                int studentID;
-
-                cout << "Enter studentID for deleting: ";
-                cin >> studentID;
-
-            int removeIdx;
-            for (int i = 0; i < students.size(); i++) {
-                if (students[i].getStudentID() == studentID) {
-                    removeIdx = i;
-                }
-            };
-
-            if (removeIdx != -1) {
-                students.erase(students.begin()+ removeIdx); 
-                // erase accept two params 1. student/begin() target to
-                // first element of students vector array which is student object.
-                // 2. second params accept int value. RemoveIdx is index of elements for deletion
-                cout << "Remove student successfully!" << "\n";
-            } else {
-                cout << "StudentID is incorrect!" << "\n";
-            }
-            };
-
-       void displayStudents() {
-        ifstream infile("student.dat");  // Declare and open the file at the same time
-
-        if (!infile.is_open()) {  // Check if the file has been successfully opened
-            cout << "File does not exist or cannot be opened!\n";
-            return;  // Exit the function
-        }
-
-        if (infile.peek() == ifstream::traits_type::eof()) { // check whether data has in file
-            cout << "There is no data within student.dat!\n";
-            infile.close();
-            return;  // Exit the function
-        }
-
-        string prefix;
+class Student {
+    private:
         string name;
         int studentID;
+    public:
+        Student(string name_val = " ", int studentID_val = 0)
+        : name(name_val), studentID(studentID_val) {};
+    
 
-        while (true) {
-            // Read "Name:" prefix
-            infile >> prefix;
-            if (!infile) break;  // Break out of the loop if reading failed (end of file reached)
-
-            // Read the name until "StudentID:"
-            getline(infile, name, 'S');  // Delimiter set to 'S' assuming "StudentID:" comes next
-            infile >> prefix;  // Read "StudentID:"
-
-            // Remove trailing spaces from the name (caused by reading up to 'S')
-            name = name.substr(0, name.length() - 1);
-
-            // Read student ID
-            infile >> studentID;
-
-            cout << "Name: " << name << ", Student ID: " << studentID << "\n";
+        string getName() {
+            return name;
         }
 
-            infile.close();  // Close the file after use
-        };
-            vector<Student> getStudentsArray() {
-                return students;
-            };
+        void setName(string name_val) {
+            name = name_val;
+        }
 
-            void setStudents(vector<Student> studs) {
-                this ->students = studs;
-            };
+        int getStudentID() {
+            return studentID;
+        };
+
+        void setStudentID(int studentID_val) {
+            studentID = studentID_val;
+        };
+
+        string toString() {
+            return "Name: " + name + ", StudentID: " + std::to_string(studentID);
+        }
 };
 
-        
+
+
+class SchoolSystem {
+private:
+        vector<Student> studentVct;
+public:
+        SchoolSystem() {};
+
+        void addStudent() {
+            string name;
+            int studentId;
+            cout << "Enter student name: ";
+            std::getline(cin >> std::ws, name);
+            cout << "Enter student ID: ";
+            cin >> studentId;
+            // create student object after ask user input
+            Student student_obj(name,studentId);
+            // add student object to student vector list
+            studentVct.push_back(student_obj);
+            cout << "Add student successfull!" << "\n\n";
+
+            // Create myFile for adding student object into student.dat
+            fstream myFile;
+            myFile.open("student.dat", std::ios::app); // append mode in order to add each student obj to student.dat
+            if (!myFile.is_open()) {
+                cerr << "File path is not exist!" << "\n";
+                return;
+            }
+            myFile << student_obj.toString() << "\n";
+            myFile.close();
+            cout << "Add student to file successfull!" << "\n";           
+        };
+    
+
+    void removeStudent() {
+    int studentID;
+    cout << "Enter student ID that you would like to remove: ";
+    cin >> studentID;
+
+    // Read and update the file
+    vector<string> lines;
+    string line;
+    bool studentFound = false; // variable to track if studentID is found
+
+    fstream myFile("student.dat", std::ios::in);
+    if (myFile.is_open()) {
+        while (getline(myFile, line)) {
+            if (line.find("StudentID: " + std::to_string(studentID)) != string::npos) {
+                studentFound = true; // studentID found in the line
+                // skip the remaining iterator and move to new iterator
+                // skip adding this line to lines vector
+                continue;
+            }
+            lines.push_back(line);
+        }
+        myFile.close();
+
+        if (!studentFound) {
+            cerr << "Student ID not found." << "\n";
+            return; // Exit the function if studentID is not found
+        }
+
+        // Write updated data back to the file
+        myFile.open("student.dat", std::ios::out | std::ios::trunc);
+        for (const auto& l : lines) {
+            myFile << l << "\n";
+        }
+        myFile.close();
+        cout << "Erased student successfully from file!" << "\n";
+    } else {
+        cerr << "Unable to open file." << "\n";
+    }
+    };
+
+
+
+        // Read student.dat and print out in console
+        void viewStudents() {
+
+            string line;
+            fstream myFile("student.dat", std::ios::in);
+
+            // check whether data has in file
+            if (myFile.peek() == std::ifstream::traits_type::eof()) { 
+                cout << "There is no data within student.dat!\n";
+                myFile.close();
+                return;  // Exit the function
+        }
+
+            if (myFile.is_open()) {
+                // read each line in student.dat
+                while (getline(myFile, line)) { 
+                    // print out student information in console
+                        cout << "Student Information: " << line << "\n";
+                    }
+                myFile.close();
+            } else {
+                cerr << "Unable to open file." << "\n";
+                myFile.close();
+            }
+        };
+
+        vector<Student> getStudentVector() {
+            return studentVct;
+        }
+};
+
 int main() {
-    SchoolSystem schoolsystem; // Create this outside the loop to retain changes across iterations
-    ofstream outfile;
-    int exit = 0;
+    SchoolSystem schoolSystem;
     int choice;
 
-    while (exit == 0) {
-        cout << "School Management System" << "\n";
-        cout << "1. View all students" << '\n';
-        cout << "2. Add Student" << "\n";
-        cout << "3. Remove Student" << "\n";
+    do {
+        cout << "School Management System " << "\n";
+        cout << "1. View all students" << "\n";
+        cout << "2. Add students" << "\n";
+        cout << "3. Remove students" << "\n";
         cout << "4. Exit" << "\n";
-        cout << "Enter your choice: ";
+        cout << "Your choice: ";
         cin >> choice;
-        switch (choice) {
+
+        switch(choice) {
         case 1:
-            schoolsystem.displayStudents();
+            schoolSystem.viewStudents();
             break;
-        case 2:
-            schoolsystem.addStudent();
+        case 2: 
+            schoolSystem.addStudent();
             break;
         case 3:
-            schoolsystem.removeStudent(); // Assuming you have this function in your SchoolSystem class
+            schoolSystem.removeStudent();
             break;
         case 4:
-            outfile.open("student.dat");
-            if (!outfile.is_open()) {
-                cout << "File name is incorrect!";
-            } else {
-                for (Student student : schoolsystem.getStudentsArray()) {
-                    outfile << student.toString() << "\n"; 
-                }
-            };
-            exit = 1;
-            break; // Exit the program
+            cout << "Exit the program!" << "\n";
+            exit(0);
         default:
-            cout << "Invalid choice";
-            break;  // Recommended to add a break statement even if it's the last case
-        };
-    };
-    return 0;  // It's good practice to have a return statement at the end of main
+            cout << "Invalid choice, please choose again!";
+            break;
+        }
+    } while(choice == 1 || choice == 2 || choice == 3 || choice == 4);
+    return 0;
 };
--- module6/tutorial/tempCodeRunnerFile.cpp
+++ module6/tutorial/tempCodeRunnerFile.cpp
@@ -1,134 +1,193 @@
 #include <iostream>
-using namespace std;
+#include <string>
+#include <vector>
+#include <fstream>
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::string;
+using std::vector;
+using std::fstream;
 
 class Student {
     private:
         string name;
         int studentID;
-    
     public:
-    // initialize constructor as member initializer
-        Student(string name_val = "", int studentID = 0) : name(name_val), studentID(studentID) {
-            cout << "Constructor of Student class is created" << "\n";
-        };
-
-    // destructor when object is out of scope
-        ~Student() {
-            cout << "Destructor of Student class" << "\n";
-        };
-
-        void setName(string name_val) {
-            this -> name = name_val;
-        };
+        Student(string name_val = " ", int studentID_val = 0)
+        : name(name_val), studentID(studentID_val) {};
+    
 
         string getName() {
             return name;
-        };
+        }
 
-        void setStudentID(int studentID) {
-            this ->studentID = studentID;
-        };
+        void setName(string name_val) {
+            name = name_val;
+        }
 
         int getStudentID() {
             return studentID;
         };
-// A virtual function in C++ is a member function of a base class that can be overridden in a derived class. 
-// The primary purpose of virtual functions is to support polymorphism, 
-// allowing a base class pointer (or reference) to call the appropriate function depending on the type of the object it points to (or references).
-        virtual string toString() {
-            return "Name: " + name + " "+ "StudentID: " + to_string(studentID);
-        };
-};
 
-class Staff {
-    private:
-        string name;
-        int staffID;
-    public:
-        Staff(string name_val = "", int staffID = 0) : name(name_val), staffID(staffID) {
-            cout << "Constructor of Staff class is created" << "\n";
+        void setStudentID(int studentID_val) {
+            studentID = studentID_val;
         };
 
-        ~Staff() {
-            cout << "Destructor of Staff class" << "\n";
-        };
-
-        void setName(string name_val) {
-            this -> name = name_val;
-        };
-
-        string getName() {
-            return name;
-        };
-
-        void setStaffID(int staffID) {
-            this -> staffID = staffID;
-        };
-
-        int getStaffID() {
-            return staffID;
-        };
-
-        virtual string toString() {
-            return "Name: " + name + " " + "StaffID: " + to_string(staffID);
-        };
-};
-
-// structure of inheritance. Tutor inherit both class
-class Tutor: public Staff, public Student {
-    public:
-        void consultation() {
-            cout << "Doing consultation";
-        };
-
-// only need to inherit one class to avoid redudant
-        Tutor(string name_val, int tutorID) : Staff(name_val,tutorID) {
-            cout << "Constructor of Tutor class is created" << "\n";
-        };
-
-        ~Tutor() {
-            cout << "Destructor of Tutor" << "\n";
-        };
-
-        void setName(string name_val) {
-            Staff::setName(name_val);
-        };
-
-        string getName() {
-            return Staff::getName();
-        };
-
-        void setStaffID(int staffID) {
-            Staff::setStaffID(staffID);
-        };
-
-        int getStaffID() {
-            return Staff::getStaffID();
-        };
-        
         string toString() {
-            return Staff::toString(); 
-        };
+            return "Name: " + name + ", StudentID: " + std::to_string(studentID);
+        }
 };
 
-void showInfoStudent(Student &std) {
-    cout << std.toString() << "\n";
-}
 
-void showInfoStaff(Staff &staff) {
-    cout << staff.toString() << "\n";
-}
+
+class SchoolSystem {
+private:
+        vector<Student> studentVct;
+public:
+        SchoolSystem() {};
+
+        void addStudent() {
+            string name;
+            int studentId;
+            cout << "Enter student name: ";
+            std::getline(cin >> std::ws, name);
+            cout << "Enter student ID: ";
+            cin >> studentId;
+            // create student object after ask user input
+            Student student_obj(name,studentId);
+            // add student object to student vector list
+            studentVct.push_back(student_obj);
+            cout << "Add student successfull!" << "\n\n";
+
+            // Create myFile for adding student object into student.dat
+            fstream myFile;
+            myFile.open("student.dat", std::ios::app); // append mode in order to add each student obj to student.dat
+            if (!myFile.is_open()) {
+                cerr << "File path is not exist!" << "\n";
+                return;
+            }
+            myFile << student_obj.toString() << "\n";
+            myFile.close();
+            cout << "Add student to file successfull!" << "\n";           
+        };
+    
+
+    void removeStudent() {
+    int studentID;
+    cout << "Enter student ID that you would like to remove: ";
+    cin >> studentID;
+    bool foundInVector = false;
+
+    // Find and remove student from vector
+    // for (int i = 0; i < studentVct.size(); i++) {
+    //     if (studentVct[i].getStudentID() == studentID) {
+    //         studentVct.erase(studentVct.begin() + i);
+    //         cout << "Erase student successfully from vector!" << "\n";
+    //     }
+    // else {
+    //     cout << "No student with ID " << studentID << " found in the vector." << "\n";
+    //     return;
+    // } 
+    // }
+
+
+    // Read and update the file
+    vector<string> lines;
+    string line;
+    bool studentFound = false; // variable to track if studentID is found
+
+    fstream myFile("student.dat", std::ios::in);
+    if (myFile.is_open()) {
+        while (getline(myFile, line)) {
+            if (line.find("StudentID: " + std::to_string(studentID)) != string::npos) {
+                studentFound = true; // studentID found in the line
+                continue; // skip adding this line to lines vector
+            }
+            lines.push_back(line);
+        }
+        myFile.close();
+
+        if (!studentFound) {
+            cerr << "Student ID not found." << "\n";
+            return; // Exit the function if studentID is not found
+        }
+
+        // Write updated data back to the file
+        myFile.open("student.dat", std::ios::out | std::ios::trunc);
+        for (const auto& l : lines) {
+            myFile << l << "\n";
+        }
+        myFile.close();
+        cout << "Erased student successfully from file!" << "\n";
+    } else {
+        cerr << "Unable to open file." << "\n";
+    }
+    };
+
+
+
+        // Read student.dat and print out in console
+        void viewStudents() {
+
+            string line;
+            fstream myFile("student.dat", std::ios::in);
+
+            // check whether data has in file
+            if (myFile.peek() == std::ifstream::traits_type::eof()) { 
+                cout << "There is no data within student.dat!\n";
+                myFile.close();
+                return;  // Exit the function
+        }
+
+            if (myFile.is_open()) {
+                // read each line in student.dat
+                while (getline(myFile, line)) { 
+                    // print out student information in console
+                        cout << "Student Information: " << line << "\n";
+                    }
+                myFile.close();
+            } else {
+                cerr << "Unable to open file." << "\n";
+                myFile.close();
+            }
+        };
+
+        vector<Student> getStudentVector() {
+            return studentVct;
+        }
+};
 
 int main() {
-    Tutor t1("Lebron", 12345678);
-    showInfoStaff(t1);
-    showInfoStudent(t1);
+    SchoolSystem schoolSystem;
+    int choice;
 
-    Student stud1("Khang",39787);
-    showInfoStudent(stud1);
+    do {
+        cout << "School Management System " << "\n";
+        cout << "1. View all students" << "\n";
+        cout << "2. Add students" << "\n";
+        cout << "3. Remove students" << "\n";
+        cout << "4. Exit" << "\n";
+        cout << "Your choice: ";
+        cin >> choice;
 
-    Staff staff1("Kobe",5588);
-    showInfoStaff(staff1);
-
+        switch(choice) {
+        case 1:
+            schoolSystem.viewStudents();
+            break;
+        case 2: 
+            schoolSystem.addStudent();
+            break;
+        case 3:
+            schoolSystem.removeStudent();
+            break;
+        case 4:
+            cout << "Exit the program!" << "\n";
+            exit(0);
+        default:
+            cout << "Invalid choice, please choose again!";
+            break;
+        }
+    } while(choice == 1 || choice == 2 || choice == 3 || choice == 4);
     return 0;
-}
+};
